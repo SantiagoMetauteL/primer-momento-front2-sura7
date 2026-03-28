80a00eb2b13c46537080d425faa5551a80c924ea
RllySaM <Andreypantoja001@gmail.com>
2026-03-28 12:18:58 -0500

    Creacion de la carpeta data y el archivo data.js, junto al arreglo de peliculas y su informacion como: id, titulo,, director, genero, duracion y el poster

---
+++ samuel-pantoja/src/data/data.js
@@ -0,0 +1,35 @@
+//Areglo de datos de películas
+export const movies = [
+  {
+    id: 1,
+    titulo: "Dune: Parte Dos",
+    director: "Denis Villeneuve",
+    genero: "Ciencia ficción",
+    duracion: "166 min",
+    poster: "https://image.tmdb.org/t/p/w500/8b8R8l88Qje9dnbOEu9pS6Cp2jN.jpg"
+  },
+  {
+    id: 2,
+    titulo: "Oppenheimer",
+    director: "Christopher Nolan",
+    genero: "Drama histórico",
+    duracion: "180 min",
+    poster: "https://image.tmdb.org/t/p/w500/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg"
+  },
+  {
+    id: 3,
+    titulo: "Wonka",
+    director: "Paul King",
+    genero: "Fantasía",
+    duracion: "116 min",
+    poster: "https://image.tmdb.org/t/p/w500/qhb1qOilapbapELWrAyBvv/VG0.jpg"
+  },
+  {
+    id: 4,
+    titulo: "Killers of the Flower Moon",
+    director: "Martin Scorsese",
+    genero: "Crimen",
+    duracion: "206 min",
+    poster: "https://image.tmdb.org/t/p/w500/7pWk4rdVz8PLT7VFZRrFvGp0p4c.jpg"
+  }
+];
